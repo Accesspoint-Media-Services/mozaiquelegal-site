c8667603064398b5239c1735ea923c6565016b82
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 10:21:53 +0100

    added a 're-title' field

--- wp-content/themes/dev-mozaique/inc/theme/blocks/products/overview/overview.php
+++ wp-content/themes/dev-mozaique/inc/theme/blocks/products/overview/overview.php
@@ -1,3 +1,6 @@
+<?php 
+    $pre_title = get_field('');
+?>
 <div class="overview-block py-12 flex items-center flex-col lg:flex-row gap-y-8 gap-x-16 justify-between" data-aos="fade-zoom-in" data-aos-easing="ease-in-back">
 
     <div class="text-2xl lg:w-1/2">
@@ -5,9 +8,11 @@
         
 
         <?php $page_colour = get_field('page_colour', get_queried_object_id()); ?>
-        <span class="eyebrow-headings pr-4" style="color: <?php echo esc_attr($page_colour); ?>;">
-            overview
-        </span> 
+        <?php if ($pre_title):?>
+            <span class="eyebrow-headings pr-4" style="color: <?php echo esc_attr($page_colour); ?>;">
+                <?php echo($pre_title)?>
+            </span> 
+        <?php endif; ?>
         <?php echo wp_strip_all_tags($overview); ?>
     </div>
 
